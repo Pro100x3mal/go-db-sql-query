11a97345fc71d43e4cf70a254bff5e823f8f45f4
Pro100x3mal <pro100x3mal@gmail.com>
2025-04-28 02:42:36 +0300

    first iteration

--- main.go
+++ main.go
@@ -32,10 +32,10 @@ func main() {
 
 	// добавление нового клиента
 	newClient := Client{
-		FIO:      "", // укажите ФИО
-		Login:    "", // укажите логин
-		Birthday: "", // укажите день рождения
-		Email:    "", // укажите почту
+		FIO:      "Leo Messi",         // укажите ФИО
+		Login:    "leo10Barca",        // укажите логин
+		Birthday: "19900101",          // укажите день рождения
+		Email:    "messi10@gmail.com", // укажите почту
 	}
 
 	id, err := insertClient(db, newClient)
@@ -53,7 +53,7 @@ func main() {
 	fmt.Println(client)
 
 	// обновление логина клиента
-	newLogin := "" // укажите новый логин
+	newLogin := "leo10InterMiami" // укажите новый логин
 	err = updateClientLogin(db, newLogin, id)
 	if err != nil {
 		fmt.Println(err)
@@ -85,17 +85,32 @@ func main() {
 
 func insertClient(db *sql.DB, client Client) (int64, error) {
 	// напишите здесь код для добавления новой записи в таблицу clients
-
-	return 0, nil // вместо 0 верните идентификатор добавленной записи
+	res, err := db.Exec("INSERT INTO clients (fio, login, birthday, email) VALUES (:fio, :login, :birthday, :email)", sql.Named("fio", client.FIO), sql.Named("login", client.Login), sql.Named("birthday", client.Birthday), sql.Named("email", client.Email))
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return id, nil // вместо 0 верните идентификатор добавленной записи
 }
 
 func updateClientLogin(db *sql.DB, login string, id int64) error {
 	// напишите здесь код для обновления поля login в таблице clients у записи с заданным id
+	_, err := db.Exec("UPDATE clients SET login = :login WHERE id = :id", sql.Named("login", login), sql.Named("id", id))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func deleteClient(db *sql.DB, id int64) error {
 	// напишите здесь код для удаления записи из таблицы clients по заданному id
+	_, err := db.Exec("DELETE FROM clients WHERE id = :id", sql.Named("id", id))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
